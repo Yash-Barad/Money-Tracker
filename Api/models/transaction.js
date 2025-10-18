798634f37bfe4681df3111d22f971146e3259b2d
Yash Maheshbhai Barad <yashbarad3536@gmail.com>
2025-10-18 23:29:20 +0530

    Update transaction.js

--- Api/models/transaction.js
+++ Api/models/transaction.js
@@ -4,7 +4,9 @@ const TransactionSchema = new Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   datetime: { type: Date, required: true },
-  amount: { type: Number, required: true }, 
+  amount: { type: Number, required: true },
+  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  category: { type: String, default: 'Other' }
 });
 
 const TransactionModel = model('Transaction', TransactionSchema);
