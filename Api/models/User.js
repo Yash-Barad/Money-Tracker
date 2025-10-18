003efbf440fc9e90caf96052fd7d73ba906ed971
Yash Maheshbhai Barad <yashbarad3536@gmail.com>
2025-10-18 23:29:58 +0530

    Add files via upload

---
+++ Api/models/User.js
@@ -0,0 +1,22 @@
+const { model, Schema } = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+const UserModel = model('User', UserSchema);
+module.exports = UserModel;
